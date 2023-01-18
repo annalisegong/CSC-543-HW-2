b6171a632e581c8e80e9041510c41daf8b7f4d91
annalisegong <annalise.gong@cuw.edu>
2023-01-18 12:38:48 -0700

    correct array and LL implementation

--- array.cpp
+++ array.cpp
@@ -4,14 +4,16 @@ using namespace std;
 int main()
 {
     //array implementation of sparse matrix
-    int arr[4][5] = {{0,0,3,0,4}, {0,0,5,7,0}, {0,0,0,0,0}, {0,2,6,0,0}};
+    int arr[3][6] = {{0,0,1,1,3,3}, {2,4,2,3,1,2}, {3,4,5,7,2,6}};
+    string s[3] = {"row", "column", "value"};
 
     //display matrix
-    for(int i = 0; i < 4; i++)
+    for(int i = 0; i < 3; i++)
     {
-        for(int j = 0; j < 5; j++)
+        cout << s[i] << ": ";
+        for(int j = 0; j < 6; j++)
         {
-            cout << arr[i][j];
+            cout << arr[i][j] << " ";
         }
         cout << endl;
     }
--- linkedlist.cpp
+++ linkedlist.cpp
@@ -70,13 +70,14 @@ public:
         }
         while(currNode != NULL)
         {
-            //i < 5 because 5 = # of values that should be in a row
-            for(int i = 0; i < 5; i++)
+            //i <= 6 because 6 = # of non zero values
+            for(int i = 0; i <= 6; i++)
             {
-                cout << currNode->value << " ";
+                cout << "row: " << currNode->row << ", ";
+                cout << "column: " << currNode->column << ", ";
+                cout << "value: " << currNode->value << endl;
                 currNode = currNode->nextNode;
             }
-            cout << endl;
         }
     }
 };
@@ -91,42 +92,14 @@ int main()
     Node* D = NULL;
     Node* E = NULL;
     Node* F = NULL;
-    Node* G = NULL;
-    Node* H = NULL; 
-    Node* I = NULL;
-    Node* J = NULL;
-    Node* K = NULL;
-    Node* L = NULL;
-    Node* M = NULL;
-    Node* N = NULL;
-    Node* O = NULL;
-    Node* P = NULL;
-    Node* Q = NULL;
-    Node* R = NULL;
-    Node* S = NULL;
-    Node* T = NULL;
 
     //assign Node values(row, column, value) to allocated memory space
-    A = new Node(0,0,0);
-    B = new Node(0,1,0);
-    C = new Node(0,2,3);
-    D = new Node(0,3,4);
-    E = new Node(0,4,4);
-    F = new Node(1,0,0);
-    G = new Node(1,1,0);
-    H = new Node(1,2,5); 
-    I = new Node(1,3,7);
-    J = new Node(1,4,0);
-    K = new Node(2,0,0);
-    L = new Node(2,1,0);
-    M = new Node(2,2,0);
-    N = new Node(2,3,0);
-    O = new Node(2,4,0);
-    P = new Node(3,0,0);
-    Q = new Node(3,1,2);
-    R = new Node(3,2,6);
-    S = new Node(3,3,0);
-    T = new Node(3,4,0);
+    A = new Node(0,2,3);
+    B = new Node(0,4,4);
+    C = new Node(1,2,5);
+    D = new Node(1,3,7);
+    E = new Node(3,1,2);
+    F = new Node(3,2,6);
     
     SinglyLinkedList* list = new SinglyLinkedList();
 
@@ -136,20 +109,6 @@ int main()
     list->append(D); 
     list->append(E);
     list->append(F);
-    list->append(G);
-    list->append(H);
-    list->append(I);
-    list->append(J);
-    list->append(K);
-    list->append(L);
-    list->append(M);
-    list->append(N);
-    list->append(O);
-    list->append(P);
-    list->append(Q);
-    list->append(R);
-    list->append(S);
-    list->append(T);
 
     list->display();
     return 0;
